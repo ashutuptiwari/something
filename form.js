56b1c747ee3c15b7b90965df66b5b10a481cc06d
ashutuptiwari <ashutosh.tiwari.cd.ece22@itbhu.ac.in>
2023-06-11 21:08:17 +0530

    add a final landing page

--- form.js
+++ form.js
@@ -2,4 +2,10 @@ window.onload=function()
 {
     var jsConfetti = new JSConfetti()
     jsConfetti.addConfetti();
+    var button=document.getElementById("submit");
+    button.onmousedown=function(){
+        jsConfetti.addConfetti();
+        setTimeout(()=>{window.location.href = "final.html";},3000)
+        
+    }
 }
