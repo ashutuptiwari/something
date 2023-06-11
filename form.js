0f9610b3bfe6641fc8c916ac88fe5bfa662f00a7
ashutuptiwari <106370097+ashutuptiwari@users.noreply.github.com>
2023-06-11 19:24:08 +0530

    Update form.js

--- form.js
+++ form.js
@@ -3,15 +3,15 @@ window.onload=function()
     var jsConfetti = new JSConfetti()
     jsConfetti.addConfetti();
     document.addEventListener("DOMContentLoaded", function() {
-    var but=document.getElementById("form") 
-    but.onmousedown()=function(){
-    
+  document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
     var name = document.getElementById("name").value;
     var where = document.getElementById("where").value;
     var when = document.getElementById("when").value;
     var idea = document.getElementById("idea").value;
     var thought = document.getElementById("thought").value;
     emailjs.init("qEXEJx4R0VjzPPu8S");
+
     var emailParams = {
       name: name,
       where: where,
@@ -19,8 +19,13 @@ window.onload=function()
       idea: idea,
       thought: thought
     };
-    emailjs.send("service_ktsg0pk", "template_v0qcqkw", emailParams)
-  };
+    emailjs.send("service_s2vn9rl", "template_v0qcqkw", emailParams)
+      .then(function(response) {
+        console.log("Email sent successfully!", response);
+      }, function(error) {
+        console.error("Email sending failed:", error);
+      });
+  });
 });
 
 }
