0cbe19293993a7ba57f678b414ef76c74459ffd9
ashutuptiwari <ashutosh.tiwari.cd.ece22@itbhu.ac.in>
2023-06-13 00:26:01 +0530

    fix the phone bug

--- index.js
+++ index.js
@@ -11,7 +11,28 @@ window.onload = function()
         console.log("yay i love you too");
         window.location.href = "form.html";
     }
+    yes.ontouchend=function()
+    {
+        alert("yey it's a date <3");
+        jsConfetti.addConfetti();
+        console.log("yay i love you too");
+        window.location.href = "form.html";
+    }
     no.onmouseover=function()
+    {
+        no.style.position="absolute";
+        yes.style.position="absolute";
+        var x1=yes.style.left;
+        var y1=yes.style.top;
+        yes.style.left=x1;
+        yes.style.top=y1;
+        var x=Math.random()*(window.innerWidth-no.style.height-200);
+        var y=Math.random()*(window.innerHeight-no.style.height-200);
+        console.log(x+"\n"+y)
+        no.style.left=`${x}px`;
+        no.style.top=`${y}px`;
+    }
+    no.ontouchend=function()
     {
         no.style.position="absolute";
         yes.style.position="absolute";
